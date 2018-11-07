48af08916bb3deeed1ce753d07e0f15c8cc66f6c
kannadhaasan <kannadhaasan@outlook.com>
2018-11-07 20:46:26 +0000

    Changes in controller and post

--- currency.controller.js
+++ currency.controller.js
@@ -3,11 +3,14 @@
 var app= angular.module("currencyApp", []);
 
 app.controller("currencyCtrl", currencyFn);
-
+this.successMessage = undefined;
+      this.errorMessage = undefined;
 
 function currencyFn($http) {
 
-   $http.get('http://localhost:3000/currency')
+
+
+   $http.get('http://localhost:8080/topics')
     .then (function(response){
       console.log(response);
     })
@@ -20,14 +23,42 @@ function currencyFn($http) {
     window.location="view.html";
   }
 
-  
+
+    this.hideMode = true;
+    this.currentdate = new Date();
+    this.locale = "en-us";
+    this.month = this.currentdate.toLocaleString(this.currentdate, {month: "short"}).toUpperCase();
+    this.datetime =  this.currentdate.getDate() + "-"
+                + this.month  + "-"
+                + this.currentdate.getFullYear() + " "
+                + this.currentdate.getHours() + ":"
+                + this.currentdate.getMinutes() + ":"
+                + this.currentdate.getSeconds();
+                console.log(this.datetime);
+
+
+
+
 
   this.postToJson = function(){
+
       var userData = this.formData;
-      $http.post('http://localhost:3000/currency/', userData)
-          .then (function(response){
-            console.log(response);
-          })
+
+      $http({
+            method: 'POST',
+            url: 'http://localhost:8080/topics/',
+            headers: {  'Content-Type': 'application/json;charset=utf-8' },
+           data:  userData
+       }).then (function(response){
+        console.log(response);
+        this.successMessage = "Data Successfully Updated";
+        window.location.reload();
+      },
+      function(){
+        this.errorMessage = "There is an error. Please try again";
+      }
+    );
+
 
         }
 
