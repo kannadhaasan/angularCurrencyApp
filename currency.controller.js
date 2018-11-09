e9a83e61b2db0022f42184d5e2a5265028b53bbc
kannadhaasan <kannadhaasan@outlook.com>
2018-11-09 01:03:24 +0000

    transfer files after changes

--- currency.controller.js
+++ currency.controller.js
@@ -1,70 +1,83 @@
-(function(){
+(function () {
 
-var app= angular.module("currencyApp", []);
+	var app = angular.module("currencyApp", []);
 
-app.controller("currencyCtrl", currencyFn);
-this.successMessage = undefined;
-      this.errorMessage = undefined;
+	app.controller("currencyCtrl", currencyFn);
 
-function currencyFn($http) {
+	function currencyFn(currencyDataSvc, $http) {
+		var self = this;
+		this.successMessage = undefined;
+		this.errorMessage = undefined;
 
+		currencyDataSvc.getMessage()
+		.then(function (data) {
+			self.message1 = data;
+			console.log(self.message1);
 
+		})
 
-   $http.get('http://localhost:8080/topics')
-    .then (function(response){
-      console.log(response);
-    })
+		this.selectMessage = function (index) {
+			this.selectedMessage = this.message1[index]
+		}
 
-  this.postfun = function(){
-    window.location="post.html";
-  }
+		this.selecteduser = function (index) {
+			this.selecteduser = this.message[index];
+		}
 
-  this.viewfun = function(){
-    window.location="view.html";
-  }
+		this.postfun = function () {
+			window.location = "post.html";
+		}
 
-
-    this.hideMode = true;
-    this.currentdate = new Date();
-    this.locale = "en-us";
-    this.month = this.currentdate.toLocaleString(this.currentdate, {month: "short"}).toUpperCase();
-    this.datetime =  this.currentdate.getDate() + "-"
-                + this.month  + "-"
-                + this.currentdate.getFullYear() + " "
-                + this.currentdate.getHours() + ":"
-                + this.currentdate.getMinutes() + ":"
-                + this.currentdate.getSeconds();
-                console.log(this.datetime);
+		this.viewfun = function () {
+			window.location = "view.html";
+		}
 
 
+		this.currentdate = new Date();
+		this.locale = "en-us";
+		this.month = this.currentdate.toLocaleString(this.currentdate, {
+				month: "short"
+			}).toUpperCase();
+		this.datetime = this.currentdate.getDate() + "-"
+			 + this.month + "-"
+			 + this.currentdate.getFullYear() + " "
+			 + this.currentdate.getHours() + ":"
+			 + this.currentdate.getMinutes() + ":"
+			 + this.currentdate.getSeconds();
+		console.log(this.datetime);
+
+		this.postToJson = function () {
+
+			var userData = {
+				"userId": this.formData.userId,
+				"currencyFrom": this.formData.currencyFrom,
+				"currencyTo": this.formData.currencyTo,
+				"amountSell": this.formData.amountSell,
+				"amountBuy": this.formData.amountBuy,
+				"rate": this.formData.rate,
+				"timePlaced": this.datetime,
+				"originatingCountry": this.formData.originatingCountry
+			}
 
 
+			$http({
+				method: 'POST',
+				url: 'http://localhost:8080/messages',
+				headers: {
+					'Content-Type': 'application/json;charset=utf-8'
+				},
+				data: userData
+			}).then(function (response) {
+				console.log(response);
+				window.location.reload();
+				this.successMessage = "Data Successfully Updated";
+			},
+				function () {
+				this.errorMessage = "There is an error. Please try again";
+			});
 
-  this.postToJson = function(){
+		}
 
-      var userData = this.formData;
-
-      $http({
-            method: 'POST',
-            url: 'http://localhost:8080/topics/',
-            headers: {  'Content-Type': 'application/json;charset=utf-8' },
-           data:  userData
-       }).then (function(response){
-        console.log(response);
-        this.successMessage = "Data Successfully Updated";
-        window.location.reload();
-      },
-      function(){
-        this.errorMessage = "There is an error. Please try again";
-      }
-    );
-
-
-        }
-
-
-
-
-}
+	}
 
 })();
--- currency.service.js
+++ currency.service.js
@@ -2,15 +2,8 @@
 var app = angular.module("currencyApp");
 app.service("currencyDataSvc", function($http){
     var self = this;
-      self.createMessage = function(userData){
-            return $http.post('http://localhost:3000/currency/', userData)
-            .then(function(response){
-              console.log(response);
-            })
-      }
-
       self.getMessage = function () {
-        var promise1 = $http.get('http://localhost:3000/currency');
+        var promise1 = $http.get('http://localhost:8080/messages');
         var promise2 =  promise1.then(function (response) {
           return response.data;
 
@@ -18,5 +11,5 @@ app.service("currencyDataSvc", function($http){
           return promise2;
       }
 
-})
+});
 })();
